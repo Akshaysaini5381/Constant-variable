a52f1b08f5f529992ba79c1648196dbe79e90401
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-24 03:35:55 +0530

    Add project files.

---
+++ Constant variable and Readonly variable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Constant_variable_and_Readonly_variable
+{
+    class Program
+    {
+        // constant variable
+        const int num = 100;
+        Program()
+        {
+            Console.WriteLine("Constructor Calling :" + num);
+        }
+        void show()
+        {
+            Console.WriteLine("Non-Static Method :"+num);
+        }
+        static void show1()
+        {
+            Console.WriteLine("Static Method :" + num);
+        }
+
+                                 
+        static void Main(string[] args)
+        {
+        
+            // constructor calling
+            Program obj = new Program();
+
+            // non-static method calling
+            obj.show();
+            // static method calling
+            Program.show1();
+
+            // constant variable  ya local variable 
+            const string name = "akshay";
+            Console.WriteLine("Constant Variable :"+num);
+            Console.WriteLine("Local Variable :" + name);
+
+        }
+    }
+}
